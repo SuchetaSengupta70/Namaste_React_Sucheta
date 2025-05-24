cd26a224633aa37d3d48c2d4e6647325e88a259c
Sucheta Sengupta <senguptasucheta17@gmail.com>
2025-05-24 20:33:07 +0530

    [sucheta - ingition of App - ep2]

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div",
     {
         class: "parent"
@@ -6,20 +9,20 @@ const heading = React.createElement("div",
             class: "child1"
         }, [React.createElement("h1",
             {
-                class: "heading"
+                className: "heading"
             }, "Hello World From React"), React.createElement("h2",
                 {
-                    class: "heading1"
+                    className: "heading1"
                 }, "Hello World From React")]),
     React.createElement("div",
         {
             class: "child2"
         }, [React.createElement("h1",
             {
-                class: "heading"
+                className: "heading"
             }, "Hello World From React"), React.createElement("h2",
                 {
-                    class: "heading1"
+                    className: "heading1"
                 }, "Hello World From React")])
 ]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Namaste React !!
+
+# Parcel
+- DEV build
+- Local Server
+- HMR- Hot MOdule Replacement (it replaces and re renders the app automqatically)
+- Using File Watching Algorithm written in C++;
+- Caching for faster builds
+- Image optimization
+- Minification
+- Bundling
